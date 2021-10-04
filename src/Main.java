3eb335fad4b124a2b58ac61600fca4f8d931883f
Vasyl <chervinskyi.v@gmail.com>
2021-10-04 13:36:51 +0300

    added mas_print method

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         print(args[0]);
+        int count = Integer.valueOf(args[1]);
+        mas_print(args[0], count);
     }
 
     public static void print(String input_line) {
         System.out.println(input_line);
     }
+    public static void mas_print(String input_line, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(input_line);
+        }
 }
