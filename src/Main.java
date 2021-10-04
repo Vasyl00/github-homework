9bb0665926fcc59fe4cf13c7adb8286c4994603a
Vasyl <chervinskyi.v@gmail.com>
2021-10-04 13:36:10 +0300

    added print method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        print(args[0]);
+    }
+
+    public static void print(String input_line) {
+        System.out.println(input_line);
     }
 }
